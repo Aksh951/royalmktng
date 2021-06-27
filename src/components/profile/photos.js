d0a939b24bb882bc32e40657a1af3e472d2b0a0f
Aksh951 <40739254+Aksh951@users.noreply.github.com>
2021-06-27 12:32:02 +0530

    a

--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import Skeleton from 'react-loading-skeleton';
 import { Tree, TreeNode } from 'react-organizational-chart';
 
-export default function Photos({ photos, lwing, rwing }) {
-  console.log(photos);
+export default function Photos({ photos }) {  
   return (
     <div className="h-16 border-t border-gray-primary mt-12 pt-4">
       <div className="grid grid-cols-3 gap-8 mt-4 mb-12">
@@ -67,7 +66,5 @@ export default function Photos({ photos, lwing, rwing }) {
 }
 
 Photos.propTypes = {
-  photos: PropTypes.array,
-  lwing: PropTypes.array,
-  rwing: PropTypes.array
+  photos: PropTypes.array  
 };
