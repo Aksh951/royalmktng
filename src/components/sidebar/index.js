328e92414baaa80b4e85045981125401b044c7d9
Aksh951 <40739254+Aksh951@users.noreply.github.com>
2021-06-27 12:36:11 +0530

    a

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -5,8 +5,7 @@ import LoggedInUserContext from '../../context/logged-in-user';
 
 export default function Sidebar() {
   return (
-    <div className="p-4">
-      <User username={username} fullName={fullName} />
+    <div className="p-4">    
       {/* <Suggestions userId={userId} following={following} loggedInUserDocId={docId} /> */}
     </div>
   );
